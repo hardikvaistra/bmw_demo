0f1a79c6d958e3a628d31445ab9adc380cab4117
Hardik <108472559+hardikvaistra@users.noreply.github.com>
2023-12-21 18:02:35 +0530

    save

--- lib/screens/fan_screen.dart
+++ lib/screens/fan_screen.dart
@@ -1,5 +1,6 @@
 import 'package:bmw_demo/constants/my_colors.dart';
 import 'package:bmw_demo/constants/my_font_icons.dart';
+import 'package:bmw_demo/constants/my_textstyle.dart';
 import 'package:bmw_demo/widgets/background_container.dart';
 import 'package:bmw_demo/widgets/my_button.dart';
 import 'package:flutter/material.dart';
@@ -46,6 +47,10 @@ class FanScreen extends StatelessWidget {
                 ),
               ),
               Expanded(child: Column()),
+              Expanded(child: Column()),
+              const Text("Fan Speed", style: ktBody),
+              const MyProgressBar(totalSize: 5, currentSize: 4),
+              const Text("Mode", style: ktBody),
               Padding(
                 padding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
@@ -83,6 +88,119 @@ class FanScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MyProgressBar extends StatelessWidget {
+  const MyProgressBar({
+    super.key,
+    required this.currentSize,
+    required this.totalSize,
+  });
+
+  final int currentSize;
+  final int totalSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              height: 10,
+              width: ((currentSize / totalSize) *
+                      MediaQuery.of(context).size.width) +
+                  30,
+              margin: const EdgeInsets.all(8),
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: BoxDecoration(
+                gradient: const LinearGradient(
+                  colors: [kcSecondaryEnd, kcSecondaryStart],
+                ),
+                borderRadius: BorderRadius.circular(5),
+              ),
+            ),
+          ),
+          Container(
+            height: 10,
+            margin: const EdgeInsets.all(8),
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(
+                totalSize,
+                (index) => Text(
+                  "${index + 1}",
+                  style: const TextStyle(fontSize: 9),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: ((currentSize / totalSize) *
+                    MediaQuery.of(context).size.width) +
+                30,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.black,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.white10,
+                        offset: Offset(-4.67, 0),
+                        blurRadius: 60.34,
+                        spreadRadius: 0.4,
+                      ),
+                      BoxShadow(
+                        color: Colors.white30,
+                        offset: Offset(-4.67, -4.67),
+                        blurRadius: 10.34,
+                        spreadRadius: 0.6,
+                      ),
+                      BoxShadow(
+                        color: Colors.black,
+                        offset: Offset(4.67, 4.67),
+                        blurRadius: 5.34,
+                        spreadRadius: 0.6,
+                      ),
+                      BoxShadow(
+                        color: Colors.white30,
+                        blurRadius: 5.34,
+                        spreadRadius: -10.6,
+                      ),
+                    ],
+                  ),
+                ),
+                AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  width: 38,
+                  height: 38,
+                  alignment: Alignment.center,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [kcBlack, kcBGGrey],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                    ),
+                  ),
+                  child: Text((currentSize + 1).toString()),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
